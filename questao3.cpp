7ace60ab107c3e532123174db803c5d44849abab
ewertonsantos <ewertonredsnop@gmail.com>
2017-03-08 06:41:38 -0300

    questao 3 finalmente

---
+++ questao3.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main(){
+
+int n,d, i;
+cout << "Ola, digite o numero de vezes que deseja jogar o dado: " << endl ;
+cin >> n  ;
+
+    for(i==0,i<n, i++){
+    srand(time(NULL));
+    d = (rand() % 6);
+    d++;
+    cout << "O numero foi: " << %d;
+    }
+
+return (0);
+}
